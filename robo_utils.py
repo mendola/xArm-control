3a2e1c6de04774dd43cdde1f9cce5dfd71116611
Ben Bonenfant <bonenfan5ben@gmail.com>
2018-10-23 23:46:07 -0400

    type annotations and naming for utils

--- robo_utils.py
+++ robo_utils.py
@@ -1,21 +1,24 @@
-def get_high_bits(val):
-        return (val & 0xFF00) >> 8
+def get_high_bits(bits: int) -> int:
+        return (bits & 0xFF00) >> 8
 
-def get_low_bits(val):
-        return val & 0xFF
 
-def deg_to_bits(degrees):
-        val = round((degrees + 120.0) * 1000.0 / 240.0)
-        if val > 1000:
-                val = 1000
-        elif val < 0:
-                val = 0
-        return val
+def get_low_bits(bits: int) -> int:
+        return bits & 0xFF
 
-def bits_to_deg(bits):
-    if bits > 1000:
-        bits = 1000
-    elif bits < 0:
-        bits = 0
-    deg = float(bits) * 240 / 1000 - 120
-    return deg
+
+def deg_to_bits(degrees: float) -> int:
+        rotation = round((degrees + 120.0) * 1000.0 / 240.0)
+        if rotation > 1000:
+                rotation = 1000
+        elif rotation < 0:
+                rotation = 0
+        return rotation
+
+
+def bits_to_deg(rotation: int) -> float:
+    if rotation > 1000:
+        rotation = 1000
+    elif rotation < 0:
+        rotation = 0
+    degrees = (rotation * 240 / 1000) - 120
+    return degrees
